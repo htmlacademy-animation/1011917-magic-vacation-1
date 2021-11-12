cbcfc1fd131956c0c8249dc8a5d9cd771bd726c3
Иван Капусткин < >
2021-11-12 10:32:58 +0300

    'done'

--- source/js/modules/three/bubbleRawShaderMaterial.js
+++ source/js/modules/three/bubbleRawShaderMaterial.js
@@ -1,7 +1,10 @@
-export default (texture) => ({
+export default (texture, _options) => ({
   uniforms: {
     map: {
       value: texture
+    },
+    options: {
+      value: _options
     }
   },
   vertexShader: `
@@ -19,11 +22,24 @@ export default (texture) => ({
   fragmentShader: `
     precision mediump float;
     uniform sampler2D map;
-    uniform float noiseAmplitude;
-    uniform float noiseSize;
     varying vec2 vUv;
+    uniform float shiftHue;
+    struct optionsStruct {
+      float hue;
+    };
+    uniform optionsStruct options;
+    vec3 hueShift(vec3 color, float hue) {
+      const vec3 k = vec3(0.57735, 0.57735, 0.57735);
+      float cosAngle = cos(hue);
+      return vec3(color * cosAngle + cross(k, color) * sin(hue) + k * dot(k, color) * (1.0 - cosAngle));
+    }
     void main() {
       vec4 texel = texture2D( map, vUv );
-      gl_FragColor = texel;
+      if (options.hue != 0.0) {
+        vec3 hueShifted = hueShift(texel.rgb, options.hue);
+        gl_FragColor = vec4(hueShifted.rgb, 1);
+      } else {
+        gl_FragColor = texel;
+      }
     }`
 });
