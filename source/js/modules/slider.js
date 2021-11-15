5ba483a21851460c05fba40120fcfacbb3874b3b
Иван Капусткин < >
2021-11-15 11:30:15 +0300

    done-m5-t4

--- source/js/modules/slider.js
+++ source/js/modules/slider.js
@@ -1,8 +1,9 @@
 import Swiper from "swiper";
-import Story from './three/story.js';
+import Story from './three/Story.js';
 
 export default () => {
   let storySlider;
+  let storyKey = false;
   const story = new Story();
 
   const setSlider = function () {
@@ -75,17 +76,19 @@ export default () => {
     if (storySlider) {
       storySlider.destroy();
     }
-    document.body.addEventListener(`screenChanged`, (e) => {
-      if (e.detail.screenName === `story`) {
-        story.setScene(0);
-      }
-    });
+
     setSlider();
+
+    if (storyKey) {
+      story.setScene(0);
+    }
   });
 
   document.body.addEventListener(`screenChanged`, (e) => {
     if (e.detail.screenName === `story`) {
       story.init();
+
+      storyKey = true;
 
       if (storySlider.activeIndex === 0 || storySlider.activeIndex === 1) {
         story.setScene(0);
