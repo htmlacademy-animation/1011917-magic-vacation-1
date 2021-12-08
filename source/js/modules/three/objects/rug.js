2041b8a4e3a75fba9404aa52a27dab0ff23390e8
ximera405 <kapustkin2011@mail.ru>
2021-12-08 11:13:58 +0300

    'add-mashes'

--- source/js/modules/three/objects/rug.js
+++ source/js/modules/three/objects/rug.js
@@ -7,21 +7,17 @@ class Rug extends THREE.Group {
     super();
 
     this.colorBase = 0x7a5ab2;
-    this.colorStrips = 0x583f8c;
 
     this.baseMesh = 0;
 
     this.startDeg = 16;
     this.finishDeg = 74;
 
-    this.lengthStrip = (this.finishDeg - this.startDeg) / 7;
-
     this.constructChildren();
   }
 
   constructChildren() {
     this.addBase();
-    this.addStrips();
   }
 
   addBase() {
@@ -32,18 +28,6 @@ class Rug extends THREE.Group {
     this.baseMesh = new THREE.Mesh(base, setMaterial({color: this.colorBase, flatShading: true}));
 
     this.add(this.baseMesh);
-  }
-
-  addStrips() {
-    for (let index = 1; index < 6; index += 2) {
-      const points = getLathePointsForCircle(180, 3, 763);
-      const {start, length} = getLatheDegrees(this.startDeg + this.lengthStrip * index, this.startDeg + this.lengthStrip * (index + 1));
-
-      const sprip = new THREE.LatheBufferGeometry(points, 5, start, length);
-      const mesh = new THREE.Mesh(sprip, setMaterial({color: this.colorStrips, flatShading: true}));
-      mesh.position.set(0, 1, 0);
-      this.add(mesh);
-    }
   }
 }
 
---
+++ source/js/modules/three/storyScenes/storyScene3.js
@@ -0,0 +1,49 @@
+import * as THREE from 'three';
+
+import SVGObject from '../svgObject.js';
+import Rug from '../objects/rug.js';
+import Chandelier from '../objects/chandelier.js';
+
+class StoryScene3 extends THREE.Group {
+  constructor() {
+    super();
+
+    this.isDark = true;
+
+    this.constructChildren();
+  }
+
+  constructChildren() {
+    this.loadFlower();
+    this.addRug();
+    this.addChandelier();
+  }
+
+  async loadFlower() {
+    const flower = await new SVGObject(`flower-storyScene3`).getObject();
+    const scale = 0.7;
+    flower.position.set(-220, 150, 200);
+    flower.scale.set(scale, -scale, scale);
+    flower.rotation.copy(new THREE.Euler(0, 40 * THREE.Math.DEG2RAD, 5 * THREE.Math.DEG2RAD), `XYZ`);
+    this.add(flower);
+  }
+
+  addRug() {
+    const rug = new Rug();
+    const scale = 0.7;
+
+    rug.scale.set(scale, scale, scale);
+    rug.position.set(0, -115, 0);
+    rug.rotation.copy(new THREE.Euler(13 * THREE.Math.DEG2RAD, -52 * THREE.Math.DEG2RAD, 0), `XYZ`);
+    this.add(rug);
+  }
+
+  addChandelier() {
+    const chandelier = new Chandelier(this.isDark);
+
+    chandelier.position.set(50, 240, 100);
+    this.add(chandelier);
+  }
+}
+
+export default StoryScene3;
